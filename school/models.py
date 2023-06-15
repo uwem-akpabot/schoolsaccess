fed1c5319f07d3a24dfd14a6e3a16f448ca99e46
Uwem Akpabot <akpabot.it@gmail.com>
2023-06-16 00:09:13 +0100

    basic models setup for schoolsaccess

--- school/models.py
+++ school/models.py
@@ -1,59 +1,85 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class School(models.Model):
+	LEVEL = (
+		('Primary', 'Primary'), 
+		('Secondary', 'Secondary'), 
+		('All', 'All'), 
+    )
+	COUNT = (
+		('0-6', '0-6'), 
+		('7-15', '7-15'), 
+		('Above 15', 'Above 15'), 
+    )
+    # patient = models.ForeignKey(Patient, related_name='soapnote', on_delete=models.CASCADE)
+	name = models.CharField(max_length=50)
+	address = models.TextField(blank=True, null=True)
+	state = models.CharField(blank=True, null=True)
+	phone = models.CharField(max_length=12, blank=True, null=True)
+	email = models.CharField(max_length=100, blank=True, null=True)
+	slogan = models.CharField(max_length=45, blank=True, null=True)
+	contact_person = models.CharField(max_length=35)
+	contact_person_phone = models.CharField(max_length=24)
+	school_level = models.CharField(max_length=10, choices=LEVEL)
+	class_count = models.CharField(max_length=10, blank=True, null=True, choices=COUNT)
+	school_colours = models.CharField(max_length=45, blank=True, null=True)
+	created_by = models.CharField(max_length=15)
+	date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	# date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)
+	# logo
 
-"""
-1. SCHOOL
-*name
-address
-*state
-phone
-email
-slogan
-logo
-*contact_person
-*contact_person_phone
-*school levels: nurs, prim, jun sec, sen sec
-class count: 0-6, 7-15, above 15 e.g. jss1 is one class, jss2 is another class; regardless of A, B, C, D sections within the classes
-*primary_color
-secondary_color
+class Sesssion(models.Model):
+	SESSIONS = (
+		('2022/2023', '2022/2023'), 
+		('2023/2024', '2023/2024'), 
+		('2024/2025', '2024/2025'), 
+		('2025/2026', '2025/2026'), 
+		('2026/2027', '2026/2027'), 
+		('2027/2028', '2027/2028'), 
+		('2028/2029', '2028/2029'), 
+		('2029/2030', '2029/2030'), 
+		('2030/2031', '2030/2031'), 
+    )
+	school = models.ForeignKey(School, related_name='sesssion', on_delete=models.CASCADE)
+	name = models.CharField(max_length=6, choices=SESSIONS)
+	is_current = models.BooleanField(default=False)
+	duration_start = models.CharField(max_length=20, blank=True, null=True) 
+	duration_end = models.CharField(max_length=20, blank=True, null=True)
+	created_by = models.CharField(max_length=15)
+	date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	# date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)
+    
+class Term(models.Model):
+	TERMS = (
+		('Primary', 'Primary'), 
+		('Secondary', 'Secondary'), 
+		('All', 'All'), 
+    )
+	school = models.ForeignKey(School, related_name='term', on_delete=models.CASCADE)
+	name = models.CharField(max_length=6, choices=TERMS)
+	is_current = models.BooleanField(default=False)
+	duration_start = models.CharField(max_length=20, blank=True, null=True) 
+	duration_end = models.CharField(max_length=20, blank=True, null=True)
+	created_by = models.CharField(max_length=15)
+	date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	# date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)
 
-2. TERM
-*name
-duration
-set_as_current (choices 0, 1)Yes, No
-*school (foreignKey)
+class Classs(models.Model):
+	school = models.ForeignKey(School, related_name='classs', on_delete=models.CASCADE)
+	sesssion = models.ForeignKey(Sesssion, related_name='classs', on_delete=models.CASCADE)
+	term = models.ForeignKey(Term, related_name='classs', on_delete=models.CASCADE)
+	name = models.CharField(max_length=25) 
+	subclass = models.CharField(max_length=25, blank=True, null=True) 
+	created_by = models.CharField(max_length=15)
+	date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	# date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)
 
-3. SESSION
-*name
-duration
-set_as_current (choices 0, 1)Yes, No
-*school (foreignKey)
-
-4. STUDENT
-*fname
-mname
-*sname
-*gender
-parent1
-*parent1_phone
-parent2
-parent2_phone
-address
-email
-*school (foreignKey)
-curr_classs (foreignKey)
-subjects (ManyToMany)
-passport
-
-3. CLASSS
-*name
-level
-curr_session (foreignKey)
-curr_term (foreignKey)
-
-4. SUBJECT
-*name
-level
-orientation
-"""
+class Subject(models.Model):
+	school = models.ForeignKey(School, related_name='subject', on_delete=models.CASCADE)
+	classs = models.ForeignKey(Classs, related_name='subject', on_delete=models.CASCADE)
+	name = models.CharField(max_length=30)
+	orientation = models.CharField(max_length=15, blank=True, null=True) 
+	created_by = models.CharField(max_length=15)
+	date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	# date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)
--- student/models.py
+++ student/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from school.models import School, Classs
 
-# Create your models here.
+class Student(models.Model):
+	GENDER = (
+		('Female', 'Female'), 
+		('Male', 'Male'), 
+		('Other', 'Other'), 
+    )
+	school = models.ForeignKey(School, related_name='student', on_delete=models.CASCADE)
+	classs = models.ForeignKey(Classs, related_name='student', on_delete=models.CASCADE)
+	fname = models.CharField(max_length=45)
+	mname = models.CharField(max_length=45)
+	sname = models.CharField(max_length=30)
+	gender = models.CharField(max_length=6, choices=GENDER)
+	phone = models.CharField(max_length=12, blank=True)
+	email = models.CharField(max_length=45, blank=True)
+	address = models.CharField(max_length=150, blank=True)
+	contact_person = models.CharField(max_length=60, blank=True, null=True)
+	contact_person_phone = models.CharField(max_length=30, blank=True)
+	created_by = models.CharField(max_length=15)
+	date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	# date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)
+	# logo
+
+"""
+4. STUDENT
+*fname
+mname
+*sname
+*gender
+parent1
+*parent1_phone
+parent2
+parent2_phone
+address
+email
+*school (foreignKey)
+curr_classs (foreignKey)
+subjects (ManyToMany)
+passport
+"""
